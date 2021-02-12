24bfc96572d419616fdaa805a7f6257dd0f3690f
Viktoria <zaharova181114@gmail.com>
2021-02-12 19:56:56 +0200

    add block

--- app/js/common.js
+++ app/js/common.js
@@ -58,6 +58,55 @@ function accordeon() {
 
 accordeon();
 
+// бегущая строка
+$('.webTicker').webTicker({
+   speed: 100,
+   duplicate: true
+});
+
+// модальные окна (несколько)
+$(function () {
+   let overlay = $('.overlay'),
+       open_modal = $('.open_modal'),
+       close = $('.modal__close, .overlay'),
+       modal = $('.modal__div');
+
+   open_modal.on('click',function (event) {
+      event.preventDefault();
+
+      modal.css('display', 'none').animate({
+         opacity: 0,
+         top: '45%'
+      }, 200);
+
+      let div = $(this).attr('href');
+      overlay.fadeIn(400,
+          function () {
+             $(div)
+                 .css('display', 'flex')
+                 .animate({
+                    opacity: 1,
+                    top: '50%'
+                 }, 200);
+          });
+   });
+
+   close.on('click', function () {
+      modal
+          .animate({
+                 opacity: 0,
+                 top: '45%'
+              }, 200,
+              function () {
+                 $(this).css('display', 'none');
+                 overlay.fadeOut(400);
+              }
+          );
+   });
+});
+//end
+
+
 // mask phone
 $('[name="phone"]').mask('+7(999) 999-99-99');
 
